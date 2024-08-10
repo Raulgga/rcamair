eda98d4cb21f70c200719a4f0a78b317ad5d9569
Sky net <g.raul.ga@protonmail.com>
2024-08-10 20:55:19 +0200

    KAN-19 Add modal functionality

---
+++ js/utils.js
@@ -0,0 +1,3 @@
+const openModal = () => document.querySelector('.menu-main').classList.add("menu-main--active");
+
+const closeModal = () => document.querySelector('.menu-main').classList.remove("menu-main--active");
